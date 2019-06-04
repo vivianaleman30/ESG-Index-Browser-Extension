f4e8d15c60b4ec83d224c0f72dbd831fe3158a95
taharihrogers <45777031+taharihrogers@users.noreply.github.com>
2019-06-03 21:38:27 -0400

    Create popup.js
    This file fixes the issue of links in the menu needing to be right-clicked. There are also some changes that are needed for popup.html

---
+++ extension/popup.js
@@ -0,0 +1,13 @@
+//this file fixes the link issue
+document.addEventListener('DOMContentLoaded', function () {
+    var links = document.getElementsByTagName("a");
+    for (var i = 0; i < links.length; i++) {
+        (function () {
+            var ln = links[i];
+            var location = ln.href;
+            ln.onclick = function () {
+                chrome.tabs.create({active: true, url: location});
+            };
+        })();
+    }
+});
